57d3d4e006fef65c5bce3f88840dc6165155b2e4
biti13012000 <biti13012k@gmail.com>
2018-10-13 14:15:21 +0700

    lan 1

---
+++ src/BiTi/ToanTu.java
@@ -0,0 +1,19 @@
+package BiTi;
+
+public class ToanTu {
+	public static void main(String args[]) {
+	int a, b;
+	a = 15;
+	b= 6;
+	System.out.println("tong="+(a+b));
+	System.out.println("hieu="+(a-b));
+	System.out.println("nhan="+(a*b));
+	System.out.println("chia="+(a/b));
+	System.out.println("chia lay du="+(a%b));
+	System.out.println("tang 1 ="+(++a));
+	System.out.println("giam 1 ="+(--b));
+	System.out.println("so sanh =" +(a==b));
+
+	}
+
+}
